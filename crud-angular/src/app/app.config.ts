0d33392cb25b7a25e730d44f0cb8dd4cb3c8333c
ivan luis <ivanlfsilva@ufrn.edu.br>
2024-02-01 11:39:08 -0300

    adicionnado roteamento aos modulos

--- crud-angular/src/app/app.config.ts
+++ crud-angular/src/app/app.config.ts
@@ -4,7 +4,6 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { MatToolbarModule } from '@angular/material/toolbar';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes), provideClientHydration(), provideAnimationsAsync()]
